39b797ead045d8ed40bfb74049d5cd274f68a7aa
samuelereso <samuelereso@yahoo.com>
2022-12-08 12:42:48 -0800

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Prints the sizes of different variables in bytes
+ * Return: 0
+ */
+int main(void)
+{
+
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));   
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
