536c6c5d3c032eaabac2ee1dafadf40a5b2f0c73
samuelereso <samuelereso@yahoo.com>
2022-12-08 11:34:12 -0800

    4 and 5 pushed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *  *main - Uses printf to print a string
+ *   *
+ *    *Description: We have been tasked with using printf
+ *     *Return: Shouldn't return anything (0)
+ *      */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
