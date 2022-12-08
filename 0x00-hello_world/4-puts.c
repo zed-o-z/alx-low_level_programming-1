752b7c088cd344d32f9db7c0ac33123ca82e08c6
samuelereso <samuelereso@yahoo.com>
2022-12-08 12:18:22 -0800

    betty cheked

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Uses puts function to print a line
- * Description: We are unable to use printf so must use put to print this string
+ * Description: We are unable to use printf so must use puts
  * Return: Code should return a 0
  */
 int main(void)
