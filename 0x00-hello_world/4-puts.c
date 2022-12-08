97e6db8ca0264297753b3d44d829085eb21b28ac
samuelereso <samuelereso@yahoo.com>
2022-12-08 11:45:15 -0800

    comment pushed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
-/**
- *main - Uses puts function to print a line
- * 
- *Description: printf is not allowed to be used
- * Return: 0 to return 
- */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
