629ae6e941a90ed437183b6d2214951c5594ee1f
samuelereso <samuelereso@yahoo.com>
2022-12-13 15:09:44 -0800

    nested loop tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - adds two integer
+ *@n: first integer
+ *@m: second integr
+ *Return: sum of integers
+ */
+int add(int n, int m)
+{
+int sum = n + m;
+
+return (sum);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_last_digit - prints the last digit of a number
+ * @n: the digit we need to find the last number of
+ *Return: value of last digit
+ */
+
+int print_last_digit(int n)
+{
+int last = n % 10;
+if (last < 0)
+last = last * -1;
+_putchar('0' + last);
+return (last);
+}
