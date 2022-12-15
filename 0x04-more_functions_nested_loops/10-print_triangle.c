ffee2d23ec37b54a92550ceab67634afa076d859
samuelereso <samuelereso@yahoo.com>
2022-12-15 13:14:41 -0800

    task for nested loop

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *print_triangle - Prints a triangle
+ *@size: the size of the triangle to print
+ *Return: nothing
+ */
+void print_triangle(int size)
+{
+int i, j;
+
+if (size > 0)
+{
+
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+if (j <= size - i)
+_putchar(' ');
+else
+{
+_putchar(35);
+if (j == size)
+_putchar('\n');
+}
+}
+}
+}
+else
+_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - multiplies two integers
+ *@a: the first integer to multiply
+ *@b: the second integer to multiply
+ *Return: the result
+ */
+
+int mul(int a, int b)
+{
+int mul = a * b;
+
+return (mul);
+}
