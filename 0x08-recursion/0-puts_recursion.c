b3b6149fb60a3ed4cab5db775d888c2433068611
samuelereso <samuelereso@yahoo.com>
2023-01-04 11:03:21 -0800

    new project

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,13 @@
 
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
+if (*s != '\0')
 {
-_putchar('\n');
-return;
-}
 _putchar(*s);
 _puts_recursion(s + 1);
+return;
+}
+else
+{
+_putchar('\n');
 }
